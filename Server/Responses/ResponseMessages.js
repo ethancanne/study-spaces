5d9d0b23b853917022a67e2b99cac6d4046d57c0
Cameron Burkholder <cburkholder19@gmail.com>
2021-11-18 11:32:04 -0500

    Organize response messages

--- Server/Responses/ResponseMessages.js
+++ Server/Responses/ResponseMessages.js
@@ -8,12 +8,12 @@ const ResponseMessages = {
     ErrorCreateAccount: "An error occurred while attempting to create an account.",
     IncorrectPassword: "The password provided is incorrect.",
     SuccessAccountCreated: "The account was created successfully.",
+    SuccessAccountSetup: "The account was setup successfully.",
     SuccessLogin: "The login was successful.",
     SuccessUpdateAuthenticationToken: "The authentication token has been successfully updated.",
+    UnverifiedUserWasFound: "The unverified user was found.",
     UserAlreadyExists: "A user with that email address already exists.",
     UserNotFound: "The user was not found.",
-    UnverifiedUserWasFound: "The unverified user was found.",
-    SuccessAccountSetup: "The account was setup successfully."
   }
 };
 module.exports = ResponseMessages;
