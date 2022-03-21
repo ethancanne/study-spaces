4753493b28f4b0b28e6aa343e27e5e561e1fd2b6
Stacey P <staceypopenfoose@gmail.com>
2022-03-21 15:52:18 -0400

    Update ViewMemberView.js

--- Client/src/Views/StudyGroup/Members/ViewMemberView.js
+++ Client/src/Views/StudyGroup/Members/ViewMemberView.js
@@ -3,8 +3,23 @@ import Button from "../../../core/Button/Button";
 import ButtonTypes from "../../../core/Button/ButtonTypes";
 import ProfilePicture from "../../../components/ProfilePicture/ProfilePicture";
 import "./ViewMemberView.scss";
+import { sendPostRequest } from "../../../../Helper";
+import Routes from "../../../../../Server/Routes/Routes";
+import ResponseMessages from "../../../../../Server/Responses/ResponseMessages";
+import { useDispatch } from "react-redux";
+import { closePopup } from "../../../state/actions";
+import { useHistory } from "react-router";
 
 const ViewMemberView = ({ member }) => {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const createConversation = () => {
+        sendPostRequest(Routes.Message.CreateConversation, {receiverId: member._id}, ResponseMessages.Message.SuccessCreateConversation, null, true, (data, error)=>{
+            if (error) return; 
+            dispatch(closePopup());
+            history.push("/message");
+        })
+    }
     return (
         <div className="view-member-view-container">
             <div className="member-details">
@@ -19,7 +34,7 @@ const ViewMemberView = ({ member }) => {
 
             <hr />
             <div className="options side-by-side">
-                <Button type={ButtonTypes.Creation}>Message</Button>
+                <Button type={ButtonTypes.Creation} onClick={createConversation}>Message</Button>
                 <Button type={ButtonTypes.Destrucive}>Report</Button>
             </div>
         </div>
